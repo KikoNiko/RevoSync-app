c570cdf5f5048f1f39658ce4f2c3b4033ef0f80f
Kiril Nikolov <kirqkov2309@gmail.com>
2024-08-26 12:50:03 +0300

    fist commit

---
+++ src/main/java/com/kbc/kibi_coins/KiBiCoinsApplication.java
@@ -0,0 +1,13 @@
+package com.kbc.kibi_coins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiBiCoinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiBiCoinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kbc/kibi_coins/KiBiCoinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbc.kibi_coins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiBiCoinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
