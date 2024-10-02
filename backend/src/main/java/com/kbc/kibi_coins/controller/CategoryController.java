d806cc36b050140db4233bd740c8260055451435
Kiril Nikolov <kirqkov2309@gmail.com>
2024-10-02 13:10:54 +0300

    modified category selection and other small improvements

---
+++ backend/src/main/java/com/kbc/kibi_coins/controller/CategoryController.java
@@ -0,0 +1,2 @@
+package com.kbc.kibi_coins.controller;public class CategoryController {
+}
---
+++ backend/src/main/java/com/kbc/kibi_coins/model/dto/CategoryDto.java
@@ -0,0 +1,13 @@
+package com.kbc.kibi_coins.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryDto {
+
+    private Long id;
+    private String name;
+
+}
