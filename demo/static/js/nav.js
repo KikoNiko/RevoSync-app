8f08a332e2537dc0960c77eb5aee35249d127c8c
Kiril Nikolov <kirqkov2309@gmail.com>
2024-11-05 16:58:17 +0200

    some work on ui

--- demo/static/js/nav.js
+++ demo/static/js/nav.js
@@ -1,12 +1,20 @@
-const nav = document.querySelector(".nav");
+const nav = document.querySelector('.nav');
   let lastScrollY = window.scrollY;
 
-  window.addEventListener("scroll", () => {
+  window.addEventListener('scroll', () => {
     if (lastScrollY < window.scrollY) {
-      nav.classList.add("nav--hidden");
+      nav.classList.add('nav--hidden');
     } else {
-      nav.classList.remove("nav--hidden");
+      nav.classList.remove('nav--hidden');
     }
 
     lastScrollY = window.scrollY;
   });
+
+
+const menuBtn = document.querySelector('.burger-menu');
+const menu = document.querySelector('.menu');
+
+menuBtn.onclick = () => {
+  menu.classList.toggle('open');
+}
