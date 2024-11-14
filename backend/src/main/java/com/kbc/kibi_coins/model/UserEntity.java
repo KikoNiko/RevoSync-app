4c484536e75dc0f392730cdef914e0aab69c43b3
Kiril Nikolov <kirqkov2309@gmail.com>
2024-11-14 16:03:42 +0200

    test

---
+++ backend/src/main/java/com/kbc/kibi_coins/model/UserEntity.java
@@ -0,0 +1,24 @@
+package com.kbc.kibi_coins.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user_entities")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+}
